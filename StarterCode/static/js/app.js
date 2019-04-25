602eb2fd682d2c7e893c2376cac30699fc74fcc5
Peter <46304666+peterjorphan@users.noreply.github.com>
2019-04-25 00:49:40 -0500

    populated table

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,16 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// Use d3 to append one table row `tr` for each ufo sighting object
+data.forEach((ufoSighting) => {
+    var row = tbody.append("tr");
+    // Use d3 to append 1 cell per ufo sighting value
+    Object.entries(ufoSighting).forEach(([key, value]) => {
+      // Append a cell to the row for each value in the object
+      var cell = tbody.append("td");
+      cell.text(value);
+    });
+});
