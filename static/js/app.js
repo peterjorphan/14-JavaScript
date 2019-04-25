1ca795f7940b60fbfe94b883a28c8faa9cff8ba2
Orphan <46304666+peterjorphan@users.noreply.github.com>
2019-04-25 09:02:01 -0500

    add event

--- static/js/app.js
+++ static/js/app.js
@@ -14,3 +14,21 @@ data.forEach((ufoSighting) => {
       cell.text(value);
     });
 });
+
+// Getting a reference to the button on the page with the id property set to `filter-btn`
+var button = d3.select("#filter-btn");
+
+// Getting a reference to the input element on the page with the id property set to 'datetime'
+var inputField = d3.select("#datetime");
+
+// Define filter function to trigger when the button is clicked
+function filter() {
+  console.log("A button was clicked!");
+
+  // Use d3 to see the object that dispatched the event
+  console.log(d3.event.target);
+}
+
+
+// Attach an event to the filter function
+button.on("click", filter);
